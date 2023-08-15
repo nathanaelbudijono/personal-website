17242101e983fb4fd144633622a2b72be5d0e8a3
Nathanael <Nathan@MacBook-Pro.local>
2023-08-15 21:52:51 +0700

    fixed text on project-card

--- src/components/card/project-card.tsx
+++ src/components/card/project-card.tsx
@@ -61,7 +61,6 @@ export default function ProjectCard({
             {date}
           </Typography>
         </div>
-        @next/next/no-img-element
         <img
           src="https://images.unsplash.com/photo-1691145697326-aa4fa5da7174?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80"
           alt=""
