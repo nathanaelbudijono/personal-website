4848f2a90484932f243fcfa31546bdbf76eeda43
Nathanael <Nathan@MacBook-Pro.local>
2023-08-15 14:24:35 +0700

    update z index

---
+++ src/modules/footer.tsx
@@ -0,0 +1,9 @@
+import Typography from "@/components/core/typography";
+
+export default function Footer() {
+  return (
+    <main>
+      <Typography variant="small">contact me</Typography>
+    </main>
+  );
+}
