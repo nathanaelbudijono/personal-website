306099741176ba556c9e9b6636f387e740472a80
Nathanael <Nathan@MacBook-Pro.local>
2023-08-16 08:29:00 +0700

    added project page

---
+++ src/constant/project-card-constant.tsx
@@ -0,0 +1,24 @@
+const ProjectCardConstant = {
+  cards: [
+    {
+      title: "Lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      date: "2021-04-20",
+      img: "https://images.unsplash.com/photo-1617711177653-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyZCUyMGJhY2tncm91bmQlMjBwcm9qZWN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      href: "/s",
+      nextjs: true,
+      postgre: true,
+      tailwind: true,
+    },
+    {
+      title: "Lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      date: "2021-04-20",
+      img: "https://images.unsplash.com/photo-1617711177653-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyZCUyMGJhY2tncm91bmQlMjBwcm9qZWN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      href: "/s",
+      nextjs: true,
+    },
+  ],
+};
+
+export default ProjectCardConstant;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,12 +1,37 @@
+import ProjectCard from "@/components/card/project-card";
+import { Framer } from "@/components/core/framer";
 import Layout from "@/components/core/layout";
 import Typography from "@/components/core/typography";
+import ProjectCardConstant from "@/constant/project-card-constant";
 
 export default function ProjectPage() {
+  const { cards } = ProjectCardConstant;
   return (
-    <Layout>
-      <Typography variant="h1" color="gradient">
-        Projects
-      </Typography>
+    <Layout className="max-sm:h-full">
+      <Framer>
+        <Typography variant="h1" color="gradient">
+          Projects
+        </Typography>
+        <Typography variant="small" className="mt-3">
+          Collection of my past projects.
+        </Typography>
+      </Framer>
+      <section className="w-full mt-5 grid grid-cols-2 max-sm:grid-cols-1 gap-5">
+        {cards.map((item, index) => (
+          <Framer delay={index * 0.8} key={index}>
+            <ProjectCard
+              title={item.title}
+              desc={item.desc}
+              date={item.date}
+              img={item.img}
+              nextjs={item?.nextjs}
+              postgre={item?.postgre}
+              tailwind={item?.tailwind}
+              href={item.href}
+            />
+          </Framer>
+        ))}
+      </section>
     </Layout>
   );
 }
