4ed16ea8f295d9213901c96b4bfe9a6dddb6bf9c
Nathanael <Nathan@MacBook-Pro.local>
2023-08-16 13:31:49 +0700

    add shorts content

--- src/constant/shorts-card-constant.tsx
+++ src/constant/shorts-card-constant.tsx
@@ -3,7 +3,7 @@ const ShortsCardContent = {
     {
       title: "Prevent Flashing",
       desc: "Prevent flashing when users wants to access an unautherized page.",
-      date: "2021-04-20",
+      date: "23-06-23",
       href: "/s",
       nextjs: true,
       postgre: true,
@@ -12,7 +12,7 @@ const ShortsCardContent = {
     {
       title: "Next-themes",
       desc: "Quick dark mode setup on NextJs with next-themes.",
-      date: "2021-06-25",
+      date: "18-02-23",
       href: "/s",
       nextjs: true,
       tailwind: true,
