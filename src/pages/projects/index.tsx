b3df1d3c33689e43db6a1831b51610013fc4bf43
Nathanael <Nathan@MacBook-Pro.local>
2023-08-16 08:11:58 +0700

    Add project page

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/core/layout";
+import Typography from "@/components/core/typography";
+
+export default function ProjectPage() {
+  return (
+    <Layout>
+      <Typography variant="h1" color="gradient">
+        Projects
+      </Typography>
+    </Layout>
+  );
+}
