2c6cc04939d127e35cd486e29410e1318e7804da
Nathanael <Nathan@MacBook-Pro.local>
2023-08-16 11:40:22 +0700

    add feat

--- src/modules/home/feat.tsx
+++ src/modules/home/feat.tsx
@@ -45,21 +45,21 @@ export default function Feat() {
 
 const project = [
   {
-    title: "Lorem ipsum",
+    title: "Design System",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
     date: "2021-04-20",
     img: "https://images.unsplash.com/photo-1617711177653-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyZCUyMGJhY2tncm91bmQlMjBwcm9qZWN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    href: "/s",
+    nextjs: true,
+  },
+  {
+    title: "Cloud Design System",
+    desc: "Cloud Design System is a collection of reusable components for faster development.",
+    date: "09-08-23",
+    img: "/projects/ds.png",
     href: "/s",
     nextjs: true,
     postgre: true,
     tailwind: true,
-  },
-  {
-    title: "Lorem ipsum",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    date: "2021-04-20",
-    img: "https://images.unsplash.com/photo-1617711177653-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyZCUyMGJhY2tncm91bmQlMjBwcm9qZWN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    href: "/s",
-    nextjs: true,
   },
 ];
