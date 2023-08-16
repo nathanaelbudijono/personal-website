632ea979fefdf08a39b8369170cadee2ff677b65
Nathanael <Nathan@MacBook-Pro.local>
2023-08-16 13:02:55 +0700

    update feat content

--- src/modules/home/feat.tsx
+++ src/modules/home/feat.tsx
@@ -50,7 +50,7 @@ const project = [
     title: "LNSW Ticketing",
     desc: "Ticketing is a reporting web page for LNSW users who encounter troubles.",
     date: "20-08-23",
-    img: "https://images.unsplash.com/photo-1617711177653-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyZCUyMGJhY2tncm91bmQlMjBwcm9qZWN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    img: "/projects/lnsw.png",
     href: "/s",
     nextjs: true,
     typescript: true,
