4555e7506bb0b4c1b6aad9583e62a84421a96184
Nathanael <Nathan@MacBook-Pro.local>
2023-08-17 20:54:10 +0700

    update iframe css

--- src/components/core/youtube-iframe.tsx
+++ src/components/core/youtube-iframe.tsx
@@ -5,7 +5,7 @@ export default function YouTube({ id }: { id: string }) {
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
-        className="w-full h-[45vh]"
+        className="w-full h-[45vh] rounded-md shadow-sm"
       />
     </div>
   );
