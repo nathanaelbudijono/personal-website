7e59cd08490d9c03db45b3831196d937e63a3c3a
Nathanael <Nathan@MacBook-Pro.local>
2023-08-17 16:06:25 +0700

    remove text

--- src/type/clsxm.tsx
+++ src/type/clsxm.tsx
@@ -1,7 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/** Merge classes with tailwind-merge with clsx full feature */
 export default function cn(...classes: ClassValue[]) {
   return twMerge(clsx(...classes));
 }
