91b5c3cd0806033a4912b61bf7427dea6327a8c0
Nathanael <Nathan@MacBook-Pro.local>
2023-10-20 22:15:09 +0700

    feat:add toast

--- src/components/core/copy.tsx
+++ src/components/core/copy.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import toast from "react-hot-toast";
+
 import useCopy from "@/hooks/useCopy";
 
 import { IoCopySharp } from "react-icons/io5";
@@ -26,7 +28,19 @@ export default function Copy() {
         className={`relative top-14 p-2 text-typography-100 opacity-50 transition-all duration-300 rounded-md ${
           copiedValue && "opacity-100"
         }`}
-        onClick={() => copy(value)}
+        onClick={() => {
+          copy(value);
+          toast.custom((t) => (
+            <div
+              className={`bg-secondary-300 dark:bg-secondary-400 dark:bg-opacity-10 bg-opacity-10 backdrop-blur-[2px] border border-secondary-300 dark:border-secondary-400 text-typography-100 dark:text-typography-800 px-4 py-2 shadow-sm text-sm rounded-md flex gap-2 items-center ${
+                t.visible ? "animate-enter" : "animate-leave"
+              }`}
+            >
+              <AiOutlineCheck className="text-md" />
+              Copied to clipboard.
+            </div>
+          ));
+        }}
       >
         {copiedValue ? (
           <AiOutlineCheck className="text-lg" />
