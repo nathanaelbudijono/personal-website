34d2aaf09e10c5103f03bd96046c3d8d44e5ea0a
Nathanael <nathanaelbudijono@gmail.com>
2024-09-10 19:31:00 +0700

    feat: update content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       <main className="bg-dark-100 dark:bg-light-100">
-        <section className="mx-auto px-16 max-md:px-6 relative">
+        <section className="mx-auto px-16 max-md:px-6">
           <NextNProgress
             color="#29D"
             startPosition={0.3}
